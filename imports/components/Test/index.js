8ef3a4e5e04f69a1ab555fc08f80f2eb1fd58e59
jm <jm@jmdeMac-mini.local>
2018-06-20 08:16:05 +0800

    update component

--- imports/components/Test/index.js
+++ imports/components/Test/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import styles from './index.less';
+console.log(styles);
 
-export default () => <div className={styles['font-20']}>font-20</div>
+export default () => <div className={styles['font-20']}> font-20 <span className={styles['font-30']}>font-30</span> </div>
